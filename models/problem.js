6b72e8ba3cf68faa45ab723f053533cf2a94289e
JustinPMitchell <j.mitchell.real@gmail.com>
2018-01-15 19:20:14 -0800

    added extra facebook information

--- models/problem.js
+++ models/problem.js
@@ -13,3 +13,24 @@ module.exports = (sequelize, DataTypes) => {
   }
   return problem;
 };
+
+//todo
+//delete all data bases
+//delete all models
+//delete all migrations
+
+//uninstall sequelize
+//reinstall sequelize
+
+//add models and migrations on the command line
+//adjust models to be the correct associations
+//readd fb stuff
+//db:migrate
+//pray everything works
+
+//or 2nd plan
+//change models to look like old version of sequelize
+//db:migrate
+//pray migrations go through
+
+//after one of the plans is done try heroku again
